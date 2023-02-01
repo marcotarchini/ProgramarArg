906d2bda8f3b3850ff25f9bac5c50c33493adfe9
Marco Tarchini <tarchinimarco@gmail.com>
2023-01-31 22:22:22 -0300

    confeccion de tabla de datos ingresados

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,14 @@
 /*!
 * Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
 //
 // Scripts
-// 
+*/ 
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
+  
+
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -18,7 +17,6 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
@@ -30,5 +28,46 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
+
+let table =[
+    {name:"Juan", email:"juan@mail.com", message:"Hola"}
+]; 
+
+window.onload = pushEvents;
+
+    function pushEvents(){
+        document.getElementById("form").addEventListener("submit", newData, false);
+        document.getElementById("get-table").addEventListener("click", getTable, false);
+    }
+
+    function newData(e){
+        e.preventDefault();
+
+        let userName = document.getElementById("name").value;
+        let userMail = document.getElementById("email").value;
+        let userMess = document.getElementById("message").value;
+
+        let newUser = {name: userName, email: userMail, message: userMess};
+        table.push(newUser);
+
+        console.log(table);
+    }
+
+
+    function getTable(){
+        
+        let bodyTable = document.getElementById("user-table");
+        let allTable = "";
+
+       for (let i=0; i < table.length; i++){
+        allTable += "<tr><td>" + table[i].name + "</td><td>" + table[i].email + "</td><td>" + table[i].message + "</td></tr>";   
+       }
+
+       bodyTable.innerHTML = allTable;
+       //"<tr><td>Juan</td><td>juan@mail.com</td><td>hola</td></tr>"
+    }
+
+  
+
+
